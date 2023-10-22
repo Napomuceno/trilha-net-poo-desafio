03346f6ea7fb98916c0bb087b365830fd9810b06
Napomuceno <erjeffersonnapomuceno@gmail.com>
2023-10-21 22:55:08 -0300

    conclusão do desafio proposto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO: Realizar os testes com as classes Nokia e Iphone
+            Console.WriteLine("=====================================");
+            Console.WriteLine("Smartphone Nokia:");
+            Smartphone nokia = new Nokia(numero: "27998087-2873", modelo: "FE ultra model one", imei: "278912851256", memoria: 128);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Dio.me");
+            Console.WriteLine("=====================================");
+
+            Console.WriteLine("Smartphone iPhone:");
+            Smartphone iphone = new Iphone(numero: "28988087-2873", modelo: "iphone 15", imei: "278912851257", memoria: 32);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("linkedin");
+            Console.WriteLine("=====================================");
+
+        }
+
+
+    }
+
+}
+
+
+
